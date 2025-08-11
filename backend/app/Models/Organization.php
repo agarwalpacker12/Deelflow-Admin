682a9be3baa73bf2b730f2b2e286cea0f694ddd1
Arkodeep Ghosh <arkodeep@kurious.one>
2025-08-11 12:24:02 +0000

    Full RBAC implementation restructured along with separating user specific endpoints

--- backend/app/Models/Organization.php
+++ backend/app/Models/Organization.php
@@ -37,4 +37,14 @@ class Organization extends Model
     {
         return $this->hasMany(User::class);
     }
+
+    public function roles()
+    {
+        return $this->hasMany(Role::class);
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class);
+    }
 }
